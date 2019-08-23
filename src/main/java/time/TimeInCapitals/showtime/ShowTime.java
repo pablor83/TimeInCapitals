fccc7341430f5a661a70ff1b74663af6ed9569a4
pablor83 <42538659+pablor83@users.noreply.github.com>
2019-08-23 19:12:43 +0200

    Added RequestMapping

--- src/main/java/time/TimeInCapitals/showtime/ShowTime.java
+++ src/main/java/time/TimeInCapitals/showtime/ShowTime.java
@@ -2,9 +2,12 @@ package time.TimeInCapitals.showtime;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,11 +20,32 @@ public class ShowTime {
 		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("H:m:s");
 		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("d MM y");
 		LocalDateTime localDateTime = LocalDateTime.now();
-		
+
 		String localTime = localDateTime.format(timeFormat);
 		String localDate = localDateTime.format(dateFormat);
+
+		return "Local Time: " + localTime + "\nLocal date: " + localDate;
+	}
+
+	@RequestMapping("/gettime")
+	@ResponseBody
+	public String getQueryParameter(@RequestParam(required = false) String td) {
 		
-		return "Local Time: "+localTime+"\nLocal date: "+localDate;
+		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/y");
+
+		LocalDateTime localDateTime = LocalDateTime.now();
+
+		String localTime = localDateTime.format(timeFormat);
+		String localDate = localDateTime.format(dateFormat);
+		String locadTimeAndDate = "Local Time: " + localTime + "\nLocal date: " + localDate;
+		
+		HashMap<String, String> timeAndDate = new HashMap<>();
+		
+		timeAndDate.put("time", localTime);
+		timeAndDate.put("date", localDate);
+				
+		return td == null ? locadTimeAndDate : timeAndDate.get(td) + " " + td;
 	}
 
 }
