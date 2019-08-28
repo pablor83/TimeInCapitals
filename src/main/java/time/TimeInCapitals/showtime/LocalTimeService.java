fe22003902ccf280575973d4b6f4af4dbd5b7415
pablor83 <42538659+pablor83@users.noreply.github.com>
2019-08-28 18:44:32 +0200

    little corrections

--- src/main/java/time/TimeInCapitals/showtime/LocalTimeService.java
+++ src/main/java/time/TimeInCapitals/showtime/LocalTimeService.java
@@ -10,33 +10,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LocalTimeService {
-	
-	private LocalDateTime localDateTime = LocalDateTime.now();
-	
+
 	public String getTime() {
-		
+
 		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
-		
+
+		LocalDateTime localDateTime = LocalDateTime.now();
+
 		String localTime = localDateTime.format(timeFormat);
-		
+
 		return localTime;
-		
+
 	}
-	
+
 	public String getDate() {
 		
 		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/y");
-		String localDate = localDateTime.format(dateFormat);
 		
+		LocalDateTime localDateTime = LocalDateTime.now();
+		
+		String localDate = localDateTime.format(dateFormat);
+
 		return localDate;
 	}
 
-	
 	public String getTimeAndDate() {
-		
+
 		String localTimeAndDate = "Local Time: " + getTime() + "\nLocal date: " + getDate();
 
 		return localTimeAndDate;
 	}
-	
+
 }
