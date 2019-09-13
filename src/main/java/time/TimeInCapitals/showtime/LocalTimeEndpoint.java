78a321a6b1f1a2afb03a8a0fc518399d51da7447
pablor83 <42538659+pablor83@users.noreply.github.com>
2019-09-13 21:06:47 +0200

    Added endpoint for European capitals

--- src/main/java/time/TimeInCapitals/showtime/LocalTimeEndpoint.java
+++ src/main/java/time/TimeInCapitals/showtime/LocalTimeEndpoint.java
@@ -1,14 +1,13 @@
 package time.TimeInCapitals.showtime;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -33,6 +32,26 @@ public class LocalTimeEndpoint {
 			throw new Exception("Bad request");
 		}
 
+	}
+	
+	@GetMapping("/europe/{capitalCity}")
+	@ResponseBody
+	public String getCapitalsTime(@PathVariable String capitalCity) {
+		
+		String capitalsOfEurope = "Europe/"+changeTheFirstLetterToUppercase(capitalCity);
+		
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		Instant instant = Instant.now();
+		ZonedDateTime zoneDataTime = instant.atZone(ZoneId.of(capitalsOfEurope));
+		return zoneDataTime.format(dateTimeFormatter);
+		
+	}
+	
+	public String changeTheFirstLetterToUppercase(String word) {
+		
+		StringBuilder stringBuilder = new StringBuilder();		
+		String wordAfterChange = (stringBuilder.append(word.substring(0, 1).toUpperCase()).append(word.substring(1))).toString();		
+		return wordAfterChange;
 	}
 
 }
